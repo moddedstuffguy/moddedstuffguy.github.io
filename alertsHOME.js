a43744a3cd04cf6cf43bda13b5615ec0c5d372e2
Modded Stuff GUY <114933191+moddedstuffguy@users.noreply.github.com>
2022-11-10 18:20:49 +0000

    Update and rename alerts1.js to alertsHOME.js

--- alertsHOME.js
+++ alertsHOME.js
@@ -1,3 +1,4 @@
 alert("Welcome to\nMODDEDSTUFFGUY'S MOD INDEX!");
 alert("Here you are going to find stuff that has been modified by ModdedStuffGuy!");
+alert("But also you are going to find some stuff that hasn't been modified\nBut this not modded stuff are things I like to use!")
 alert("Hope you enjoy with the stuff!\n=D");
