284381386f734ca3f7951c83afb0179513965405
Modded Stuff GUY <114933191+moddedstuffguy@users.noreply.github.com>
2022-11-16 18:07:04 +0000

    Create LocalStorageEditor.js

---
+++ LocalStorageEditor.js
@@ -0,0 +1,4 @@
+// Made by ModdedStuffGuy
+var LocalStorageTXT = prompt("Please, enter a value or string\nFOR ENTERING A STRING, please, enter it inside double or single quotes");
+var LocalStorageName = prompt("Enter the name of the localstorage you wanna edit or create");
+localStorage[LocalStorageName] = LocalStorageTXT;
